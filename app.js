56d0a5be59e54ea4549be015dcbd8a22b3125472
Milan Stojko <milanmilance2000.ms@gmail.com>
2022-01-13 17:52:26 +0100

    vue completo

---
+++ app.js
@@ -0,0 +1,7 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Maledetti Scarafaggi',
+      img: "https://guidatv.sky.it/uuid/6df6aa8e-0ed7-4776-a712-e6da16470946/cover?md5ChecksumParam=2152360bdd4a34f51d04932c0e3c00e3"
+    }
+  })
